5593e890ec7fe2eb44af697f9b7552cd8f099c41
Anthony White <awhite@dataxu.com>
2015-04-07 09:47:53 -0400

    feat(Model): Implement a simple model class

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,74 @@
-module CatwalkJs {
+///<reference path="../typings/tsd.d.ts" />
 
-  export class HelloWorld {
+/**
+ * Usage:
+ *
+ * var Campaign = Catwalk.Model.create('campaign', {
+ *   uid: {
+ *     type: 'string'
+ *   },
+ *   name: {
+ *     type: 'string',
+ *     maxLength: 255
+ *   }
+ * });
+ *
+ * var fordCampaign = Campaign.$create({
+ *   name: 'Ford1'
+ * });
+ **/
 
-    constructor() {
-      alert('Hello World');
-    }
+module Catwalk {
 
+  interface IModelOptions extends Object {
+    registerAdapter?(store : JSData.DS) : void;
   }
 
+  class Model {
+    private _store : JSData.DS;
+    private _model : Object;
+    private _resource : any;
+    private _errors : Object;
+
+    constructor(store : JSData.DS, resource : any) {
+      this._store = store;
+      this._resource = resource;
+      this._model = properties;
+    }
+
+    $create(properties : Object) {
+      var promise = this._resource.DSCreate(properties);
+
+      promise.catch((err) => {
+        this._errors = err;
+      });
+
+      return promise;
+    }
+
+    static create(resourceName : string, schema : Object, options : IModelOptions = {}) {
+      var store = new JSData.DS();
+
+      if (typeof options.registerAdapter !== undefined) {
+        options.registerAdapter(store);
+      } else {
+        store.registerAdapter('localstorage', new DSLocalStorageAdapter(), { default: true });
+      }
+
+      var resource = store.defineResource({
+        name: resourceName,
+        schema: schema
+      });
+
+      return new Model(store, resource);
+    }
+
+    get model() {
+      return this._model;
+    }
+
+    get errors() {
+      return this._errors;
+    }
+  }
 }
